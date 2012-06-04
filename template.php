c4f1124124627cfc637f228a7cbf19ccfa5c6179
Drew Kennelly <drewkennelly@gmail.com>
2012-06-04 22:48:21 +1000

    Initial commit

--- template.php
+++ template.php
@@ -1,13 +1,13 @@
 <?php
 
-	/*
-		Preprocess function to determine number of columns for main content area
-	*/
-	function foundation_preprocess_page(&$variables) {
-		if ( !empty($variables['page']['left']) && !empty($variables['page']['right']) )
-			$variables['main_columns'] = 'six';
-		else if ( !empty($variables['page']['left']) || !empty($variables['page']['right']) )
-			$variables['main_columns'] = 'nine';
-		else if ( empty($variables['page']['left']) && empty($variables['page']['right']) )
-			$variables['main_columns'] = 'twelve';
-	}
+  /*
+    Preprocess function to determine number of columns for main content area
+  */
+  function foundation_preprocess_page(&$variables) {
+    if ( !empty($variables['page']['left']) && !empty($variables['page']['right']) )
+      $variables['main_columns'] = 'six';
+    else if ( !empty($variables['page']['left']) || !empty($variables['page']['right']) )
+      $variables['main_columns'] = 'nine';
+    else if ( empty($variables['page']['left']) && empty($variables['page']['right']) )
+      $variables['main_columns'] = 'twelve';
+  }
